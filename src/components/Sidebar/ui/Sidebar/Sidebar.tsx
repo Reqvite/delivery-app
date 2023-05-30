7bf6c71a1394f3d5d55151ec2cae3ff5dd3c1894
Reqvite <reqvite2233@gmail.com>
2023-05-30 15:53:46 +0300

    add translation

--- src/components/Sidebar/ui/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/ui/Sidebar/Sidebar.tsx
@@ -1,9 +1,11 @@
+import { useTranslation } from 'react-i18next';
 import { useLocation } from 'react-router-dom';
 
 import { CategoriesList } from '../CategoriesList/CategoriesList';
 import cls from './Sidebar.module.scss';
 
 export const Sidebar = () => {
+    const {t} =useTranslation()
     const { pathname } = useLocation()
 
     if (pathname !== '/') {
@@ -14,7 +16,7 @@ export const Sidebar = () => {
             <div
                 className={cls.sidebar}
             >
-                <h1 className={cls.title}>All stores</h1>
+                <h1 className={cls.title}>{t('All stores')}</h1>
                 <CategoriesList />
             </div>
             <div className={cls.box} />
